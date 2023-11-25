d119b26a78bd7b59a2b0d7d7a72dae4461c616b1
Fsociety <almvetmundoanimal@gmail.com>
2023-11-25 11:49:54 -0500

    fix conexion

--- Data/conexion.py
+++ Data/conexion.py
@@ -7,14 +7,15 @@ class Conexion():
     def create():
         try:
             conexion = mysql.connector.connect(
-                host='localhost',
+                host='54.226.201.49',
                 port=3306,
                 user='backend',
                 password='1234',
                 db='appointmenthub'
             ) 
+            return conexion
         except Error as ex:
             print("Error al intentar la conexión: {0}".format(ex))
-        return conexion
+        
 
     
